2e2a86a09f7b2850f60014fdd185dbe86e26501f
Owen Rapaport <owenssrapaport@aol.com>
2015-04-01 10:18:26 -0400

    added CRUD routes for users and interests, and established m-to-m routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Libraries
-var express = require("express"),
+var application_root = __dirname,
+    express = require("express"),
     logger = require("morgan"),
     bodyParser = require("body-parser"),
     models = require("./models"),
@@ -14,4 +15,165 @@ var app = express();
 app.use(logger("dev"));
 app.use(bodyParser());
 app.use(express.static(__dirname + "/public"));
+
+//User Routes
+
+app.get("/users", function(req, res) {
+  User
+  .findAll({include: [Interest] })
+  .then(function(users) {
+    res.send(users);
+  });
+});
+
+app.get("/users/:id", function(req, res) {
+  var userID = req.params.id;
+  User
+  .findOne({
+    where: {id: userID},
+    include: [Interest]
+  })
+  .then(function(user) {
+    res.send(user);
+  });
+});
+
+app.post("/users", function(req, res) {
+  var userParams = req.body;
+  User
+  .create(userParams)
+  .then(function(newUser) {
+    res.send(newUser);
+  });
+});
+
+app.put("/users/:id", function(req, res) {
+  var userID = req.params.id;
+  var userParams = req.body;
+  User
+  .findOne(userID)
+  .then(function(user) {
+    user
+    .update(userParams)
+    .then(function(updatedUser) {
+      res.send(updatedUser);
+    });
+  });
+});
+
+app.delete("/users/:id", function(req, res) {
+  var userID = req.params.id;
+  User
+  .findOne(userID)
+  .then(function(user) {
+    user
+    .destroy()
+    .then(function() {
+      res.send(user);
+    });
+  });
+});
+
+//Adding and removing interests from users
+
+app.put("/users/:id/add_interest", function(req, res) {
+  var userID = req.params.id
+  var interestID = req.body.interest_id;
+
+  User
+  .findOne(userID, { include: [Interest] })
+  .then(function(user) {
+    Interest
+    .findOne(interestID)
+    .then(function(interest) {
+      user
+      .addInterest(interest)
+      .then(function(info) {
+        res.send(info);
+      });
+    });
+  });
+});
+
+app.put("/users/:id/remove_interest", function(req, res) {
+  var userID = req.params.id;
+  var interestID = req.body.interest_id;
+
+  User
+  .findOne(userID, {include: [Interest]})
+  .then(function(user) {
+    Interest
+    .findOne(interestID)
+    .then(function(interest) {
+      user
+      .removeInterest(interest)
+      .then(function(info) {
+        res.send(info);
+      });
+    });
+  });
+});
+
+//Interest Routes
+
+app.get("/interests", function(req, res) {
+  Interest
+  .findAll({ include: [User] })
+  .then(function(interests) {
+    res.send(interests);
+  });
+});
+
+app.get("/interests/:id", function(req, res) {
+  var interestID = req.params.id;
+
+  Interest
+  .findOne(interestID)
+  .then(function(interest) {
+    res.send(interest);
+  });
+});
+
+app.post("/interests", function(req, res) {
+  var interestParams = req.body;
+
+  Interest
+  .create(interestParams)
+  .then(function(newInterest) {
+    res.send(newInterest);
+  });
+});
+
+app.put("/interests/:id", function(req, res) {
+  var interestID = req.params.id;
+  var interestParams = req.body;
+  Interest
+  .findOne(interestID)
+  .then(function(interest) {
+    interest
+    .update(interestParams)
+    .then(function(updatedInterest) {
+      res.send(updatedInterest);
+    });
+  });
+});
+
+app.delete("/interests/:id", function(req ,res) {
+  var interestID = req.params.id;
+  Interest
+  .findOne(interestID)
+  .then(function(interest) {
+    interest
+    .destroy()
+    .then(function() {  
+      res.send(interest);
+    });
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server running on 3000");
+});
+
+
 
