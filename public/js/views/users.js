6ac4fabc7cb7f7497cdf20cfedc237d0e07f298b
ossrapaport <ossrapaport@users.noreply.github.com>
2015-04-03 10:10:13 -0400

    Update users.js

--- public/js/views/users.js
+++ public/js/views/users.js
@@ -1,14 +1,16 @@
 App.Views.Users = Backbone.View.extend({
+	//Needs an el
   initialize: function() {
   	console.log('all users View');
     this.listenTo(this.collection, 'reset', this.renderAll);
+    //Needs a template
   },
 
   renderAll: function() {
     this.collection.each(this.renderOne, this)
   }
 }),
-
+	//Needs to take an argument "user"
 	renderOne: function() {
 		this.$el.html(this.template(this.model.toJSON() ));
 	}
