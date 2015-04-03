6048a5dfcba3ecb14319b5fb9dd22e4bbbe626ed
Owen Rapaport <owenssrapaport@aol.com>
2015-04-02 20:05:06 -0400

    set up base filtering routes in the algorithm

--- algorithm.js
+++ algorithm.js
@@ -1,29 +1,60 @@
 //This fuction will return the search query to plug into Factual
 exports.buildSearchQuery = function(chosenPrice, chosenNeighborhood, typesArray) { 
-  
-  //Libraries
-  var application_root = __dirname,
-    express = require("express"),
-    logger = require("morgan"),
-    bodyParser = require("body-parser"),
-    Factual = require("factual-api"),
-    environment = require("dotenv");
 
-  //Loading libaries
-  var app = express();
-  environment.load();
-  var factual = new Factual(process.env.FACTUAL_KEY_2, process.env.FACTUAL_SECRET_2);
-  app.use(logger("dev"));
-  app.use(bodyParser());
+  var neighborhood = chosenNeighborhood.toString();
+  var price = parseInt(chosenPrice);
 
-  var info = "placeholder";
+  if (price !== 1) {
+    var base = {
+      "$and":[
+        {
+        "alcohol":"true", 
+        "meal_dinner":true, 
+        "neighborhood":
+          {
+            "$includes": chosenNeighborhood
+          }
+        },
+        {"$or":
+          [
+            {"rating":"4"},
+            {"rating":"5"},
+            {"rating":"4.5"}
+          ]
+        },
+        {"$or":
+          [
+            {"price":price},
+            {"price": (price - 1)}
+          ]
+        }
+      ]
+    }  
+  } else if (price == 1) {
+    var base = {
+      "$and":[
+        {
+        "price":1, 
+        "alcohol":"true", 
+        "meal_dinner":true, 
+        "neighborhood":
+          {
+            "$includes": chosenNeighborhood
+          }
+        },
+        {"$or":
+          [
+            {"rating":"4"},
+            {"rating":"5"},
+            {"rating":"4.5"}
+          ]
+        }
+      ]
+    }  
+  }
 
-  factual.get('/t/restaurants-us', {filters:
-  {"$and":[{"locality":"new york", "price":3, "alcohol":"true", "meal_dinner":true, "neighborhood":{"$includes":"soho"}},{"$or":[{"rating":"4"},{"rating":"5"},{"rating":"4.5"}]}]}},
-  function (error, response) {
-    objectTest.data = response.data;
-  });
 
+  //For future information, the code below worked with server.js
   var test = {"$and":[{"price":3, "alcohol":"true", "meal_dinner":true, "neighborhood":{"$includes":"park slope"}},{"$or":[{"rating":"4"},{"rating":"5"},{"rating":"4.5"}]}]}
   return test;
 }
