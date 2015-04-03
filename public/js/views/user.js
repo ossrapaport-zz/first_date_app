b59436e712e644601b7d21ea26ed8a03357734b8
ossrapaport <ossrapaport@users.noreply.github.com>
2015-04-03 10:09:07 -0400

    Update user.js

--- public/js/views/user.js
+++ public/js/views/user.js
@@ -1,4 +1,5 @@
 App.Views.User = Backbone.View.extend({
+	//Needs an el
 	initialize: function() {
 		console.log('single user view')
 		this.template = Handlebars.compile($(#single-user-template).html());
@@ -8,14 +9,15 @@ App.Views.User = Backbone.View.extend({
 	render: function() {
 		this.$el.html( this.template(this.model.toJSON()) );
 	},
- 
+ 	//This should take an argument, "user"
   	setUser: function() {
   		this.model.set(user.toJSON() );
   		this.model.getUser();
   	},
-
+	//We can either have the function take a user model or a user model id, and then go from there. You choose which.
+	//I think an ID might be more flexible.
   	updateUser: function(updatedUser) {
-  		var updateThis = updatedUser.id;
+  	      	var updateThis = updatedUser.id;
   		var newUpdatedUser = App.collection.findWhere({id: updateThis});
   		newUpdatedUser.set({
   			username: user.username,
