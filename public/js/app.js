27e4dfd6974d0b1c2a99a4f890ec41e87e72c836
Owen Rapaport <owenssrapaport@aol.com>
2015-04-05 17:58:16 -0400

    put a few things in the app onload

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,11 @@
 $(function() {
 
   App.users = new App.Collections.Users;
-  App.usersView = new App.Views.users({ collection: App.users });
-
+  App.usersView = new App.Views.Users({ collection: App.users });
   App.users.fetch({ reset: true });
+
+  App.interests = new App.Collections.Interests;
+  App.interestsView = new App.Views.Interests({ collection: App.interests });
+  App.interests.fetch({ reset: true });
 
 });
