15986af5dcb1df8a1cc52b884393cccb090e2e50
Owen Rapaport <owenssrapaport@aol.com>
2015-04-06 12:32:57 -0400

    fleshed out modal_view and set up app and router

--- public/js/views/modal_view.js
+++ public/js/views/modal_view.js
@@ -2,37 +2,70 @@ App.Views.ModalView = Backbone.View.extend({
 	el: '#account-modal',
 	initialize: function() {
 		this.listenTo(this.model, 'change', this.render);
-		this.newUserTemplate = Handlebars.compile($('#create-user-template').html());
-		this.template = Handlebars.compile($('#account-modal-template').html());
-		
+		this.newUserTemplate = Handlebars.compile( $('#create-user-template').html() );
+		this.template = Handlebars.compile( $('#account-modal-template').html() );
 	},
-
-	events: {
-		'click #create-acct-btn':'createNewUser'
-
-	},
-
-	createNewUser: function() {
-		var newUserView = new App.Views.User({ model: user });
-		newUserView.$el.appendTo($('#account-modal'));
-
-	},
-
 	showExistingUsers: function() {
 		App.users.fetch();
 	},
-
 	show: function() {
 		this.$el.fadeIn(500);
 	},
-
 	hide: function() {
 		this.$el.fadeOut(200);
 	},
-
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
 		this.showExistingUsers();
 		this.show();
+	},
+	showNewUser: function() {
+		this.$el.html( this.newUserTemplate.html() );
+	},
+	createNewUser: function() {
+		var userName = this.$el.find( $("#username").val() );
+		var userPassword = this.$el.find( $("#password").val() )
+		var usersName = this.$el.find( $("#name").val() );
+		var userDOB = this.$el.find("#dob").val();
+		var personalityList = this.$el.find("#personality-list");
+		var userPersonality = personalityList.options[personalityList.selectedIndex].value;
+		var interestsIDArray = this.getCheckedBoxesID("interest-checkbox"); 
+		var data = {
+			username: userName,
+			password: userPassword,
+			name: usersName,
+			date_of_birth: userDOB,
+			personality: userPersonality
+		};
+		App.users.create(data);
+		var userID = App.users.last().id;
+		var count = 0;
+		interestsIDArray.forEach(function(interestID) {
+			count ++;
+			$.ajax({
+				url: "/users" + userID + "/add_interest",
+				method: "PUT",
+				data: {
+					interest_id: interestID
+				}
+			})
+			if (count === interestsIDArray.length) {
+				App.searchView.render();
+				//TODO: Check what this is here
+				this.hide();
+			}
+		}).bind(this);
+		App.router.navigate("/search" + userID);
+	},
+	selectUser: function(event) {
+		var userID = $(event.currentTarget).id;
+		App.searchView.render();
+		this.hide();
+		App.router.navigate("search/" + userID);
+	},
+	events: {
+		'click #create-acct-btn':'showNewUser',
+		"click #register-btn": "createNewUser",
+		"click .user-li": "selectUser"
 	}
 })
