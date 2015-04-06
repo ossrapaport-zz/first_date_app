471a6685e84e3c79b330045e875118b59a03f669
Owen Rapaport <owenssrapaport@aol.com>
2015-04-05 20:57:11 -0400

    modified app.js, users.js, and result.js to work with search

--- public/js/views/result.js
+++ public/js/views/result.js
@@ -1,12 +1,10 @@
 App.Views.Result = Backbone.View.extend({
 	el: '#search-result',
 	initialize: function() {
-		this.template = Handlebars.compile($('#search-result').html());
-		this.render();
+		this.template = Handlebars.compile($('#search-result-template').html());
 	},
 	render: function() {
-		$('#search-results').empty();
-		this.$el.html(this.template(this.model.toJSON()))
-
+		this.$el.empty();
+		this.$el.html(this.template( this.model.toJSON() ));
 	}
 })
