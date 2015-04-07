533fa2894ac357c91c8e59311d327d4d8869090d
Owen Rapaport <owenssrapaport@aol.com>
2015-04-07 10:21:52 -0400

    making a new user view

---
+++ public/js/views/new_user.js
@@ -0,0 +1,70 @@
+App.Views.NewUser = Backbone.View.extend({
+
+  el; "#account-modal",
+  initialize: function() {
+    this.template = Handlebars.compile( $('#create-user-template').html() );
+  },
+  render: function() {
+    this.$el.html( this.newUserTemplate );
+    App.interestsView = new App.Views.Interests ({ collection: App.interests });
+    App.interestsView.renderAll();
+    App.router.navigate("newprofile");
+  },
+  getCheckedBoxesID: function(checkboxName) {
+    var findTerm = "[name=" + checkboxName + "]";
+    var checkboxes = this.$el.find(findTerm);
+    var checkedBoxesIDs = [];
+
+    for (var i = 0; i < checkboxes.length; i++) {
+       if (checkboxes[i].checked) {
+          checkedBoxesIDs.push( parseInt( checkboxes[i].value) );
+       }
+    }
+    return checkedBoxesIDs;
+  },
+  createNewUser: function() {
+    console.log("I'm here");
+    var userName = this.$el.find("#username").val().trim();
+    var userPassword = this.$el.find("#password").val().trim();
+    var usersName = this.$el.find("#name").val().trim();
+    var userDOB = this.$el.find("#dob").val().trim();
+    var personalityList = this.$el.find("#personality-list");
+    var userPersonality = personalityList.find(":selected").text();
+    var interestsIDArray = this.getCheckedBoxesID("interest-checkbox"); 
+    var data = {
+      username: userName,
+      password: userPassword,
+      name: usersName,
+      date_of_birth: userDOB,
+      personality: userPersonality
+    };
+    debugger;
+    console.log(data);
+    $.ajax({
+      url: "/users",
+      method: "POST",
+      data: data
+    }).done(function(newUserData) {
+      var userID = newUserData.id;
+      var count = 0;
+      interestsIDArray.forEach(function(interestID) {
+        count ++;
+        $.ajax({
+          url: "/users/" + userID + "/add_interest",
+          method: "PUT",
+          data: {
+            interest_id: interestID
+          }
+        })
+        if (count === interestsIDArray.length) {
+          this.hide();
+          App.searchView.render();
+        }
+      }.bind(this));
+      App.router.navigate("/search/" + userID);
+    }.bind(this));
+  },
+  events: {
+    "click #register-btn": "createNewUser"
+  }
+});
