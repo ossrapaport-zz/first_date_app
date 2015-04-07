6057750bbd6fd8e3c2ca69ccd46ae1f10edec627
Owen Rapaport <owenssrapaport@aol.com>
2015-04-07 10:21:25 -0400

    made some changes to render new user

--- public/js/views/modal_view.js
+++ public/js/views/modal_view.js
@@ -1,7 +1,7 @@
 App.Views.ModalView = Backbone.View.extend({
 	el: '#account-modal',
 	initialize: function() {
-		this.newUserTemplate = Handlebars.compile( $('#create-user-template').html() );
+		//this.newUserTemplate = Handlebars.compile( $('#create-user-template').html() );
 		this.template = Handlebars.compile( $('#account-modal-template').html() );
 	},
 	show: function() {
@@ -17,18 +17,38 @@ App.Views.ModalView = Backbone.View.extend({
 		this.show();
 	},
 	showNewUser: function() {
+		var newUserView = new App.Views.NewUser();
+		newUserView.render();
+		/*
 		this.$el.html( this.newUserTemplate );
 		App.interestsView = new App.Views.Interests ({ collection: App.interests });
 		App.interestsView.renderAll();
-		App.router.navigate("newprofile");
+		this.delegateEvents();
+		App.router.navigate("newprofile");*/
+		//Event is delegated here. Otherwise it would be delegated on render
+		//but the newTemplate wouldn't exist yet so it wouldn't work.
+		//this.delegateEvents({"click #register-btn": "createNewUser"});
 	},
+  /*getCheckedBoxesID: function(checkboxName) {
+    var findTerm = "[name=" + checkboxName + "]";
+    var checkboxes = this.$el.find(findTerm);
+    var checkedBoxesIDs = [];
+
+    for (var i = 0; i < checkboxes.length; i++) {
+       if (checkboxes[i].checked) {
+          checkedBoxesIDs.push( parseInt( checkboxes[i].value) );
+       }
+    }
+    return checkedBoxesIDs;
+  },
 	createNewUser: function() {
-		var userName = this.$el.find( $("#username").val() );
-		var userPassword = this.$el.find( $("#password").val() )
-		var usersName = this.$el.find( $("#name").val() );
-		var userDOB = this.$el.find("#dob").val();
+		console.log("I'm here");
+		var userName = this.$el.find("#username").val().trim();
+		var userPassword = this.$el.find("#password").val().trim();
+		var usersName = this.$el.find("#name").val().trim();
+		var userDOB = this.$el.find("#dob").val().trim();
 		var personalityList = this.$el.find("#personality-list");
-		var userPersonality = personalityList.options[personalityList.selectedIndex].value;
+		var userPersonality = personalityList.find(":selected").text();
 		var interestsIDArray = this.getCheckedBoxesID("interest-checkbox"); 
 		var data = {
 			username: userName,
@@ -37,28 +57,33 @@ App.Views.ModalView = Backbone.View.extend({
 			date_of_birth: userDOB,
 			personality: userPersonality
 		};
-		App.users.create(data);
-		var userID = App.users.last().id;
-		var count = 0;
-		interestsIDArray.forEach(function(interestID) {
-			count ++;
-			$.ajax({
-				url: "/users" + userID + "/add_interest",
-				method: "PUT",
-				data: {
-					interest_id: interestID
+		debugger;
+		console.log(data);
+		$.ajax({
+			url: "/users",
+			method: "POST",
+			data: data
+		}).done(function(newUserData) {
+			var userID = newUserData.id;
+			var count = 0;
+			interestsIDArray.forEach(function(interestID) {
+				count ++;
+				$.ajax({
+					url: "/users/" + userID + "/add_interest",
+					method: "PUT",
+					data: {
+						interest_id: interestID
+					}
+				})
+				if (count === interestsIDArray.length) {
+					this.hide();
+					App.searchView.render();
 				}
-			})
-			if (count === interestsIDArray.length) {
-				App.searchView.render();
-				//TODO: Check what this is here
-				this.hide();
-			}
-		}).bind(this);
-		App.router.navigate("/search/" + userID);
-	},
+			}.bind(this));
+			App.router.navigate("/search/" + userID);
+		}.bind(this));
+	},*/
 	events: {
-		'click #create-acct-btn':'showNewUser',
-		"click #register-btn": "createNewUser"
+		"click #create-acct-btn":"showNewUser"
 	}
 })
